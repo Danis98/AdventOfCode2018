cffd7aae0090b88fcc3386c27b95a23952fe3208
Daniele Venier <danielevenier1998@gmail.com>
2019-10-10 13:22:45 +0800

    Day 18

---
+++ day18/part2.py
@@ -0,0 +1,53 @@
+grid = [list(line) for line in open("day18.input", "r").read().rstrip().split("\n")]
+
+MAX_TURNS = 1000000000
+
+mem = {}
+
+turn = 1
+
+def get_neigh(r, c):
+    ctr = {'.':0, '|':0, '#':0}
+    for rr in range(max(0, r-1), min(r+2, len(grid))):
+        for cc in range(max(0, c-1), min(c+2, len(grid[rr]))):
+            if (rr, cc) == (r, c):
+                continue
+            ctr[grid[rr][cc]] += 1
+    return ctr
+
+while turn <= MAX_TURNS:
+    n_grid = [['' for i in range(len(grid[j]))] for j in range(len(grid))]
+    for r in range(len(grid)):
+        for c in range(len(grid[r])):
+            neigh = get_neigh(r, c)
+            if grid[r][c] == '.' and neigh['|'] >= 3:
+                n_grid[r][c] = '|'
+            elif grid[r][c] == '|' and neigh['#'] >= 3:
+                n_grid[r][c] = '#'
+            elif grid[r][c] == '#' and not (neigh['#'] >= 1 and neigh['|'] >= 1):
+                n_grid[r][c] = '.'
+            else:
+                n_grid[r][c] = grid[r][c]
+
+    if ''.join([''.join(line) for line in n_grid]) in mem:
+        cyc_start = mem[''.join([''.join(line) for line in n_grid])]
+        cur = turn
+        cyc_len = cur - cyc_start
+        cyc_pos = (MAX_TURNS - cur) % cyc_len
+        MAX_TURNS = cur + cyc_pos
+
+    grid = n_grid
+
+    trees = lumber = 0
+    for line in grid:
+        for e in line:
+            if e == '|':
+                trees += 1
+            elif e == '#':
+                lumber += 1
+
+    mem[''.join([''.join(line) for line in n_grid])] = turn
+
+    turn += 1
+
+print(trees*lumber)
