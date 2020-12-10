c4a6f8907b657c8cae2a2c005d0d7cdb8a2bf7d3
Daniele Venier <danielevenier1998@gmail.com>
2020-12-10 15:39:51 +0000

    Day 13

---
+++ day13/part2.py
@@ -0,0 +1,106 @@
+from queue import PriorityQueue
+
+grid = open("day13.input", "r").read().rstrip().split("\n")
+
+ROWS = len(grid)
+COLS = max([len(line) for line in grid])
+
+grid = [list(line.ljust(COLS)) for line in grid]
+
+graph = [[] for i in range(ROWS*COLS)]
+
+delta = {
+    '>': (0, 1),
+    '<': (0, -1),
+    '^': (-1, 0),
+    'v': (1, 0)
+}
+
+left = {
+    '>': '^',
+    '<': 'v',
+    '^': '<',
+    'v': '>'
+}
+
+right = {
+    '>': 'v',
+    '<': '^',
+    '^': '>',
+    'v': '<'
+}
+
+carts = PriorityQueue()
+cart_set = set()
+
+for r in range(ROWS):
+    for c in range(COLS):
+        idx = COLS * r + c
+        if grid[r][c] in '-><':
+            graph[idx].append(idx+1)
+            graph[idx].append(idx-1)
+            if grid[r][c] in '<>':
+                carts.put(((r, c), grid[r][c], 0))
+                cart_set.add((r, c))
+                grid[r][c] = '-'
+        elif grid[r][c] in '|v^':
+            graph[idx].append(idx+COLS)
+            graph[idx].append(idx-COLS)
+            if grid[r][c] in '^v':
+                carts.put(((r, c), grid[r][c], 0))
+                cart_set.add((r, c))
+                grid[r][c] = '|'
+        elif grid[r][c] == '+':
+            graph[idx].append(idx+1)
+            graph[idx].append(idx-1)
+            graph[idx].append(idx+COLS)
+            graph[idx].append(idx-COLS)
+        elif grid[r][c] == '/' or grid[r][c] == '\\':
+            if r > 0 and (grid[r-1][c] == '|' or grid[r-1][c] == '+'):
+                graph[idx].append(idx-COLS)
+            if r < ROWS-1 and (grid[r+1][c] == '|' or grid[r+1][c] == '+'):
+                graph[idx].append(idx+COLS)
+            if c > 0 and (grid[r][c-1] == '-' or grid[r][c-1] == '+'):
+                graph[idx].append(idx-1)
+            if c < COLS-1 and (grid[r][c+1] == '-' or grid[r][c+1] == '+'):
+                graph[idx].append(idx+1)
+
+end = False
+q2 = PriorityQueue()
+while not end:
+    while not carts.empty():
+        cart = carts.get()
+        r, c = cart[0][0], cart[0][1]
+        if (r, c) not in cart_set:
+            continue
+        nr, nc = r+delta[cart[1]][0], c+delta[cart[1]][1]
+        if (nr, nc) in cart_set:
+            cart_set.remove((r, c))
+            cart_set.remove((nr, nc))
+            if len(cart_set) == 1:
+                end = True
+            continue
+        cart_set.remove((r, c))
+        cart_set.add((nr, nc))
+        ndir = cart[1]
+        nctr = cart[2]
+        if grid[nr][nc] == '/':
+            if cart[1] in '<>':
+                ndir = left[cart[1]]
+            elif cart[1] in '^v':
+                ndir = right[cart[1]]
+        elif grid[nr][nc] == '\\':
+            if cart[1] in '<>':
+                ndir = right[cart[1]]
+            elif cart[1] in '^v':
+                ndir = left[cart[1]]
+        elif grid[nr][nc] == '+':
+            if cart[2] == 0:
+                ndir = left[cart[1]]
+            elif cart[2] == 2:
+                ndir = right[cart[1]]
+            nctr = (cart[2] + 1) % 3
+        q2.put(((nr, nc), ndir, nctr))
+    carts, q2 = q2, carts
+for cart in cart_set:
+    print("%d,%d" % (cart[1], cart[0]))
